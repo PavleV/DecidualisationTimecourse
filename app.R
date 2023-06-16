11d18c7bf85c1191567a5d07b73bdaecb4a3c24e
Pavle Vrljicak <a007678@007678.local>
2023-06-16 13:17:46 +0100

    atac plots

--- app.R
+++ app.R
@@ -18,12 +18,8 @@ ui <- fluidPage(
             # Choose gene name
             textInput("genename", label = h3("Gene Name"), value = NULL, placeholder="Enter gene name..."),
 
-            #hr(),
             # choose a genomic region
             textInput("genome_coord", label = h3("Genomic region"), value = NULL, placeholder="Enter chromosomal location..."),
-
-
-            #fluidRow(column(3, verbatimTextOutput("value"))),
 
             # select Biopsies
             checkboxGroupInput(inputId="which", label="Select samples:",
@@ -35,7 +31,9 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
             h3(textOutput("sampleChoice", container = span)),
-            plotOutput("distPlot")
+            plotOutput("rnaPlot"),
+            h3(textOutput("genome_coord", container = span)),
+            plotOutput("atacPlot")
         )
     )
 )
@@ -45,7 +43,7 @@ server <- function(input, output) {
 
     output$sampleChoice <- renderText(input$which, sep=", ")
 
-    output$distPlot <- renderPlot({
+    output$rnaPlot <- renderPlot({
         # draw the line plot of RNA expression based on genename/ensemblID and selected biopsies
         plotRNA.FUN(mydata = AH_EL_RNA_ALLREPS, geneName=input$genename, ensemblID=NULL, biopsies = input$which)
 
--- functions.R
+++ functions.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(ggplot2)
+library(GenomicRanges)
 
 
 # load data
@@ -12,7 +13,7 @@ AH_EL_RNA_ALLREPS <- read.csv("./Data/AH_EL_RNA_ALLREPS.csv")
 # Functions
 
 # subset data according to gene name or ensembl ID
-subset.FUN <- function(mydata=AH_EL_RNA_ALLREPS, geneName=NULL, ensemblID=NULL){
+subset.RNA.FUN <- function(mydata=AH_EL_RNA_ALLREPS, geneName=NULL, ensemblID=NULL){
 
   if(!is.null(geneName) & !is.null(ensemblID)){
     return(subset(mydata, GeneName==geneName & EnsemblID ==ensemblID))
@@ -30,7 +31,7 @@ subset.FUN <- function(mydata=AH_EL_RNA_ALLREPS, geneName=NULL, ensemblID=NULL){
 
 arrange.FUN <- function(mydata, geneName=NULL, ensemblID=NULL){
 
-  mydata.subset <- subset.FUN(mydata=mydata, geneName=geneName, ensemblID=ensemblID)
+  mydata.subset <- subset.RNA.FUN(mydata=mydata, geneName=geneName, ensemblID=ensemblID)
   mydata.subset <-  pivot_longer(mydata.subset, cols = colnames(mydata)[2:40])
 
   mydata.subset <- cbind(mydata.subset,str_split(mydata.subset$name,pattern="_", simplify = T))
@@ -67,4 +68,58 @@ plotRNA.FUN <- function(mydata, geneName=NULL, ensemblID=NULL, biopsies = c("S16
 
   print(p1)
 }
+
+
+
+#load ATAC count matrix and genomic coordinates
+
+ATAC_countsmatrix_cleaned <- read.csv("./Data/ATAC_countsmatrix_cleaned_230616.csv", row.names=1)
+
+Alex_ATAC_peaks_hg19_201110 <- read.delim("./Data/Alex_ATAC_peaks_hg19_201110.bed", header=FALSE)
+
+AllPeaks.granges <- GRanges(seqnames = Alex_ATAC_peaks_hg19_201110[,1], ranges = IRanges(start=Alex_ATAC_peaks_hg19_201110[,2], end=Alex_ATAC_peaks_hg19_201110[,3]), mcols = data.frame(PeakID = Alex_ATAC_peaks_hg19_201110[,4]))
+
+
+# function for extracting genomic coordinates from text input and generating genomic ranges object
+
+extractCoord <- function(string.input){
+
+  coordinates <- str_split_1(string.input, pattern=regex("[:,_-[:space:]]"))
+
+  return(GRanges(seqnames = coordinates[1], ranges = IRanges(start=as.numeric(coordinates[2]), end=as.numeric(coordinates[3]))))
+
+
+}
+
+# function for subsetting data matrix given a set of coordinates
+
+subset.ATAC.FUN <- function(mydata = ATAC_countsmatrix_cleaned, coordinate.key = AllPeaks.granges, coordinates){
+
+  mydata.subset <- mydata[subjectHits(findOverlaps(extractCoord(coordinates), coordinate.key)),]
+
+  mydata.subset$PeakID <- row.names(mydata.subset)
+
+  mydata.subset <-  pivot_longer(mydata.subset, cols = colnames(mydata))
+
+  mydata.subset <- cbind(mydata.subset, str_split(mydata.subset$name,pattern="_", simplify = T))
+  colnames(mydata.subset)[4:5] <- c("Biopsy","Hours")
+
+  return(mydata.subset)
+
+}
+
+
+# function for plotting ATAC peaks based on set of valid genomic coordinates
+
+ATAC.plot.data <- subset.ATAC.FUN(coordinates = "chr1:200-30000")
+
+plotATAC.FUN <- function(mydata){
+
+  p1 <- ggplot(mydata)+
+    geom_line(geom_line(aes(x=as.numeric(Hours),y=value,colour=Biopsy)))
+
+  return(print(p1))
+
+}
+
 
